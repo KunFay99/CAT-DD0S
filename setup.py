11876b607eca3d1f220837234f544431e98290d3
KunFay99 <kaffayukiffu@gmail.com>
2024-10-17 19:30:02 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("pkg install python")
+    os.system("pkg install python2")
+    os.system("pkg install fyglet")
+    
+
+elif c == "1":
+    os.system("pkg install python")
+    os.system("pkg install python2")
+    os.system("pkg install fyglet")
+    
+if os.name == "nt":
+print("Done.")
