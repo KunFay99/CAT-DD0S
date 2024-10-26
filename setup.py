a22856eda1464c833f0c0e6a8d4fa1b212d90a21
KunFay99 <kaffayukiffu@gmail.com>
2024-10-26 17:28:09 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ c = input(">>>: ")
 if c == "0":
     os.system("pkg install python")
     os.system("pkg install python2")
-    os.system("pkg install fyglet")
-    
+    os.system("pip2 install requests")
+    os.system("git pull")
 
 elif c == "1":
     os.system("pkg install python")
     os.system("pkg install python2")
-    os.system("pkg install fyglet")
+    os.system("pip2 install requests")
+    os.system("git pull")
     
 print("Done.")
