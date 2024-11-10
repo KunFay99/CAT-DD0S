7d1f7af1cd8a604b3b159e4444db6a722a2088da
KunFay99 <kaffayukiffu@gmail.com>
2024-11-11 01:05:32 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,20 @@ c = input(">>>: ")
 if c == "0":
     os.system("apt install python")
     os.system("apt install python2")
+    os.system("apt install python3-pip")
+    os.system("pip install requests")
+    os.system("apt install nodejs-lts")
+    os.system("apt install npm")
+    os.system("npm i string")
     os.system("git pull")
 
 elif c == "1":
     os.system("apt install python")
     os.system("apt install python2")
+    os.system("apt install python3-pip")
+    os.system("pip install requests")
+    os.system("apt install nodejs-lts")
+    os.system("apt install npm")
+    os.system("npm i string")
     os.system("git pull")
-    
-print("Done.")
+    print("Done.")
