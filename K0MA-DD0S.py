5bfee32e84e37173a0393827352fb5ef7128691b
KunFay99 <kaffayukiffu@gmail.com>
2024-11-11 12:30:49 +0700

    Update K0MA-DD0S.py

--- K0MA-DD0S.py
+++ K0MA-DD0S.py
@@ -34,11 +34,11 @@ print("        🟠  🟠      🟠     🟠     🟠        🟠    🟠🟠�
 print("        🔵   🔵     🔵     🔵     🔵        🔵    🔵      🔵         ")
 print("        🔵    🔵      🔵 🔵       🔵        🔵    🔵      🔵         ")              
 print(" ")
-print("\033[33m===================================================================\033[0m")
+print("\033[31m===================================================================\033[0m")
 print("\033[97m                  use this script for good purpose                       \033[0m")
 print("\033[97m                        design by: Za'99                               \033[0m")
 print("\033[97m                           --oO0Oo--                                   \033[0m")
-print("\033[33m===================================================================\033[0m")                  
+print("\033[31m===================================================================\033[0m")                  
 useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
 "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
 "Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
@@ -66,10 +66,10 @@ acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=
 "Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
 "Accept-Language: en-US,en;q=0.5\r\n"]
 
-ip = str(input('\003[93m[\033[97m+\033[93m]\033[92mTarget: \033[0m'))
-port = int(input('\003[93m[\033[97m+\033[93m]\033[92mPort: \033[0m'))
-thread = int(input('\003[93m[\033[97m+\033[93m]\033[92mThreads: \033[0m'))
-port = int(input('\003[93m[\033[97m+\033[93m]\033[92mPacket/s: \033[0m'))
+ip = str(input("\003[93m[\033[97m+\033[93m]\033[92mTarget: \033[0m"))
+port = int(input("\003[93m[\033[97m+\033[93m]\033[92mPort: \033[0m"))
+thread = int(input("\003[93m[\033[97m+\033[93m]\033[92mThreads: \033[0m"))
+port = int(input("\003[93m[\033[97m+\033[93m]\033[92mPacket/s: \033[0m"))
 
 def start():
     global useragents, ref, acceptall
